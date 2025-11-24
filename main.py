aa1abc16d5ecb0dbd18c5f5a4056e33432b03288
HyperBanner <131620724+HyperBanner@users.noreply.github.com>
2025-11-24 15:29:36 +0100

    Refactored word_count into stat file, added pycache to gitignore

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
+from stats import get_word_count
+
+
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
-
-
-def get_word_count(book):
-    split_list = book.split()
-    return len(split_list)
 
 
 def main():
---
+++ stats.py
@@ -0,0 +1,3 @@
+def get_word_count(book):
+    split_list = book.split()
+    return len(split_list)
