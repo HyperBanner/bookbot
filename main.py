be960a6251d576577cc06b186989fa37cf30df25
HyperBanner <131620724+HyperBanner@users.noreply.github.com>
2025-11-24 16:12:12 +0100

    Program now correctly handles a filepath given as a cli argument

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
+import sys
+from stats import get_book_text
 from stats import get_word_count
 from stats import get_char_count
 from stats import sort_char_count
 
 
-def get_book_text(filepath):
-    with open(filepath) as f:
-        return f.read()
-
-
 def main():
-    filepath = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    filepath = sys.argv[1]
     book = get_book_text(filepath)
     word_count = get_word_count(book)
-    char_count = get_char_count(book)
-    sorted_count = sort_char_count(char_count)
+    sorted_count = sort_char_count(get_char_count(book))
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {filepath}...")
     print("----------- Word Count ----------")
--- stats.py
+++ stats.py
@@ -1,3 +1,8 @@
+def get_book_text(filepath):
+    with open(filepath) as f:
+        return f.read()
+
+
 def get_word_count(book):
     split_list = book.split()
     return len(split_list)
